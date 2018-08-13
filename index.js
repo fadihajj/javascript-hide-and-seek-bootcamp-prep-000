4c0ba16f1b99b0570c8b34afac757aeb424f2525
Fadi hajj <fadi.elhajj@lau.edu>
2018-08-13 18:28:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
 
+function nestedTarget(){
+  return document.getElementsByTagName('div')[4];
+}
+
+function deepestChild () {
+  
+  var el = document.getElementById('grand-node');
+  var nest = el.getElementsByTagName('div')[3];
+
+  return nest;
+}
+
+function increaseRankBy(n){
+  var el =document.querySelector('.ranked-list').innerHTML;
+for (var i=0; i<=n; i++){
+el = el + '<li>2</li>';}
+}
